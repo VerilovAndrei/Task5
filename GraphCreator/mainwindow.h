228a1a20589e7cb9c7aeef41b7de52e3a62da277
VerilovAndrei <116629350+VerilovAndrei@users.noreply.github.com>
2022-10-25 12:49:32 +0500

    Add files via upload

---
+++ GraphCreator/mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QScrollArea>
+#include <QWheelEvent>
+#include <QGraphicsView>
+#include <QKeyEvent>
+#include <QMouseEvent>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void paintEvent(QPaintEvent *event);
+    void SetGraphProperty(int,double*);
+private:
+    Ui::MainWindow *ui;
+    void graph();
+
+protected:
+    virtual void wheelEvent(QWheelEvent * event);
+    void keyPressEvent(QKeyEvent *event);
+    void keyReleaseEvent(QKeyEvent *event);
+    void mouseMoveEvent(QMouseEvent* event);
+};
+#endif // MAINWINDOW_H
